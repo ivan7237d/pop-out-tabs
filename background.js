59ada26c331d47cd75e3882f86d24900c9fd6cfc
Ivan Novikov <ivan7237d@gmail.com>
2018-03-14 13:37:07 +0300

    Format and version bump

--- background.js
+++ background.js
@@ -1,21 +1,24 @@
 chrome.browserAction.onClicked.addListener(currentTab => {
-	PopoutTab(currentTab);
+  popoutTab(currentTab);
 });
 
 chrome.commands.onCommand.addListener(command => {
-	if(command == "popout_action") {
-     PopoutTab(-1);
-	}
+  if (command == 'popout_action') {
+    popoutTab(-1);
+  }
 });
 
-function PopoutTab(currentTab){
-  chrome.tabs.query({ currentWindow: true, highlighted: true },
+function popoutTab(currentTab) {
+  chrome.tabs.query(
+    { currentWindow: true, highlighted: true },
     highlightedTabs => {
-      chrome.windows.create({tabId: highlightedTabs[0].id},
-        window => {
-          chrome.tabs.move(highlightedTabs.map(tab => tab.id),
-            { windowId: window.id, index: -1 });
-          chrome.tabs.update(currentTab.id, { active: true });
+      chrome.windows.create({ tabId: highlightedTabs[0].id }, window => {
+        chrome.tabs.move(highlightedTabs.map(tab => tab.id), {
+          windowId: window.id,
+          index: -1,
         });
-    })
+        chrome.tabs.update(currentTab.id, { active: true });
+      });
+    },
+  );
 }
